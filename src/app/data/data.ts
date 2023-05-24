268abf6ee32413a5e189b3ebe15cdaacb9f80025
unknown <johnferreira141@gmail.com>
2023-05-24 07:06:24 -0300

    first commit

---
+++ src/app/data/data.ts
@@ -0,0 +1,14 @@
+export const dataArticle=[
+    {
+        "id":"1",
+        "title":"Jujutsu Kaisen Cap. 222",
+        "desc":"Lorem ipsum dolor sit amet consectetur adipisicing elit Tenetur sunt esse dolorum inventore similique consequuntur quisquam pariatur facere laudantium vero omnis fuga, nemo, enim minus impedit veritatis nihil, doloribus labore!",
+        "photoCover":"https://popularanime.com.br/wp-content/uploads/2022/10/Satoru_Gojo_Anime-768x403.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Blue Lock",
+        "desc":"TESTE DESC",
+        "photoCover":"https://i0.wp.com/sakugabrasil.com/administracao/wp-content/uploads/2021/08/E8lCD-qVcAMFDw9.jpeg"
+    },
+]
